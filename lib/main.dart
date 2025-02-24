2736bf89cb8e89df2d251d251a56aa27e3f862d4
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-02-24 18:44:37 -0500

    Fixed UI

--- lib/main.dart
+++ lib/main.dart
@@ -98,48 +98,67 @@ class MyHomePage extends StatelessWidget {
             // rebuilds if the model is updated.
             Consumer<Counter>(
               builder: (context, counter, child) => Text(
-                'I am ${counter.value} years old.',
+                'I am ${counter.value} years old',
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
             ),
+
+            const SizedBox(height: 30),
+
+            // Increment button
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
               onPressed: () {
                 context.read<Counter>().increment();
               }, 
-              child: const Text('Increment'),
+              child: const Text('Increase Age', style: TextStyle(color: Colors.white)),
             ),
+
+            // Decrement button
+            const SizedBox(height: 20),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
               onPressed: () {
                 context.read<Counter>().decrement();
               }, 
-              child: const Text('Decrement'),
+              child: const Text('Reduce Age', style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // You can access your providers anywhere you have access
-          // to the context. One way is to use Provider.of<Counter>(context).
-          // The provider package also defines extension methods on the context
-          // itself. You can call context.watch<Counter>() in a build method
-          // of any widget to access the current state of Counter, and to ask
-          // Flutter to rebuild your widget anytime Counter changes.
-          //
-          // You can't use context.watch() outside build methods, because that
-          // often leads to subtle bugs. Instead, you should use
-          // context.read<Counter>(), which gets the current state
-          // but doesn't ask Flutter for future rebuilds.
-          //
-          // Since we're in a callback that will be called whenever the user
-          // taps the FloatingActionButton, we are not in the build method here.
-          // We should use context.read().
-          var counter = context.read<Counter>();
-          counter.increment();
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     // You can access your providers anywhere you have access
+      //     // to the context. One way is to use Provider.of<Counter>(context).
+      //     // The provider package also defines extension methods on the context
+      //     // itself. You can call context.watch<Counter>() in a build method
+      //     // of any widget to access the current state of Counter, and to ask
+      //     // Flutter to rebuild your widget anytime Counter changes.
+      //     //
+      //     // You can't use context.watch() outside build methods, because that
+      //     // often leads to subtle bugs. Instead, you should use
+      //     // context.read<Counter>(), which gets the current state
+      //     // but doesn't ask Flutter for future rebuilds.
+      //     //
+      //     // Since we're in a callback that will be called whenever the user
+      //     // taps the FloatingActionButton, we are not in the build method here.
+      //     // We should use context.read().
+      //     var counter = context.read<Counter>();
+      //     counter.increment();
+      //   },
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
 }
